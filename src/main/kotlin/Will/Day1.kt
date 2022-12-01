2e2f03f7e44a15d36b758ef00ca439495dc4bd8b
Will <wilhelm.lundstrom@devoteam.com>
2022-12-01 18:42:16 +0100

    Solution for day 1 part 1

--- src/main/kotlin/Will/Day1.kt
+++ src/main/kotlin/Will/Day1.kt
@@ -2,5 +2,17 @@ import common.*
 
 fun main() {
     val data = Utilities.getInputAsStringList(TEST, 1,1)
-    print(data)
+
+    var biggest = 0
+    var tmp = 0
+    data.forEach {
+        if (it.isEmpty()) {
+            biggest = if (biggest < tmp) tmp else biggest;
+            tmp = 0
+        } else {
+            tmp += it.toInt()
+        }
+    }
+
+    println ("Solution for part 1: $biggest")
 }
